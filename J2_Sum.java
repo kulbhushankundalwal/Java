7bbf28c94ddbd3ed640f325499043d8ec6d692af
kulbhushankundalwal <kulbhushankhiladi@gmail.com>
2022-08-24 17:45:57 +0530

    first

---
+++ J2_Sum.java
@@ -0,0 +1,10 @@
+import javax.naming.StringRefAddr;
+
+public class J2_Sum {
+    public static void main(String args[]) {
+        int a = 2;
+        int b = 4;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
