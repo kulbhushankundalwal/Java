3be0eb19ea3ce019d70a4e8c000f0129a9a2ba0b
kulbhushankundalwal <kulbhushankhiladi@gmail.com>
2022-08-24 17:36:32 +0530

    First

--- Basic/J1_HelloWorld.java
+++ Basic/J1_HelloWorld.java
@@ -1,6 +1,6 @@
 class J1_HelloWorld {
     public static void main(String args[]) {
-        String a = "KK";
+        String a = "KK World";
         System.out.print("Hello World\n");
         System.out.println(a);
     }
