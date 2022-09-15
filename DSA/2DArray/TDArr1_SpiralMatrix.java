4a2823a06920d35d0f8506a7ddb9659f520a8506
kulbhushankundalwal <kulbhushankhiladi@gmail.com>
2022-09-15 20:18:57 +0530

    new

---
+++ DSA/2DArray/TDArr1_SpiralMatrix.java
@@ -0,0 +1,48 @@
+public class TDArr1_SpiralMatrix {
+
+    public static void printSM(int matrix[][]){
+        int startRow=0, startCol=0;
+        int endRow=matrix.length-1, endCol=matrix[0].length-1;
+        
+        while(startRow<=endRow && startCol<=endCol){
+            //top
+            for(int j=startCol; j<=endCol; j++){
+                System.out.print(matrix[startRow][j]+" ");
+            }
+
+            //right
+            for(int i=startRow+1; i<=endRow; i++){
+                System.out.print(matrix[i][endCol]+" ");
+            }
+
+            //bottom
+            for(int j=endCol-1; j>=startCol; j--){
+                if(startRow==endRow){
+                    break;
+                }
+                System.out.print(matrix[endRow][j]+" ");
+            }
+
+            //left
+            for(int i=endRow-1; i>=startRow+1; i--){
+                if(endCol==startCol){
+                    break;
+                }
+                System.out.print(matrix[i][startCol]+" ");
+            }
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][]={{1, 2, 3, 4},
+                    {5, 6, 7, 8},
+                    {9, 10, 11, 12},
+                    {14, 15, 16, 17},
+                    {18, 19, 20, 21}}; 
+        printSM(matrix);
+    }
+}
